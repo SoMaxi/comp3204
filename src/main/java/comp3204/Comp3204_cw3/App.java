f557ef471d621eb3b6cbe9dd01a9f2bfd5aa6398
Max.Umarovs <mu1g16@soton.ac.uk>
2018-12-09 14:45:35 +0000

    first commit

---
+++ src/main/java/comp3204/Comp3204_cw3/App.java
@@ -0,0 +1,13 @@
+package comp3204.Comp3204_cw3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
