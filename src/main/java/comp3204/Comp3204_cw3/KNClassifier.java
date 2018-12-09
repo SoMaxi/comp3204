33e60e0c0329c94b8e54629290d5677c82e6ccea
Max.Umarovs <mu1g16@soton.ac.uk>
2018-12-09 15:24:43 +0000

    fix

--- src/main/java/comp3204/Comp3204_cw3/KNClassifier.java
+++ src/main/java/comp3204/Comp3204_cw3/KNClassifier.java
@@ -1,5 +1,44 @@
 package comp3204.Comp3204_cw3;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openimaj.image.FImage;
+import org.openimaj.image.processing.resize.ResizeProcessor;
 
 public class KNClassifier {
+		
+	
+	public KNClassifier() {
+		
+	}
+	
+	public void train(List<FImage> images) {
+		 resize(images);
+		
+		
+	}
+	
+	public List<FImage> resize(List<FImage> images){
+		
+		List<FImage> newImages = new ArrayList<FImage>();
+		for(FImage img : images) {
+			
+			if(img.getHeight()>img.getWidth()) {
+				img = img.extractCenter(img.getWidth(), img.getWidth());						
+			}else if(img.getHeight()>img.getWidth()) {
+				img = img.extractCenter(img.getWidth(), img.getWidth());
+			}
+			
+			img.processInPlace(new ResizeProcessor());
+			
+		}
+		
+		
+		
+		return newImages;
+		
+		
+	}
+	
 
 }
